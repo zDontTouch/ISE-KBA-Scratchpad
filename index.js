da8bf86ee784ce43817163081cb3c2574e3e7acf
Leonardo Felini <leonardofelini9857@gmail.com>
2024-06-13 21:53:19 -0300

    Basic KBA ordering

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 
-   ise.extension.initWindow(700);
-   ise.window.setMinimumSize(700);
+   ise.extension.initWindow(750);
+   ise.window.setMinimumSize(750);
    
    let currentKbaData = [];
+   let isReorderActive = false;
 
    /* Intercept clicks which have an href - these should be opened in an ISE tab, not the extension window */
    document.addEventListener("click", (e) => {
@@ -16,6 +17,17 @@
        /* Open the URL as an ISE tab in the foreground (show:true) */
        ise.tab.add(url, { show: true });
      }
+
+     //close dropdown menu when clicking outside of it
+     if (!e.target.matches('.dropbtn') && !e.target.matches(".menuIcon")) {
+      var dropdown = document.getElementById("menuDropdown");
+      try{
+        dropdown.classList.remove("show");
+      }catch(err){
+
+      }
+    }
+
    });
 
    function copyKbaLink(id){
@@ -38,6 +50,7 @@
     
     ise.extension.sendEventToWorker('update-csv',currentKbaData);
     ise.extension.sendEventToWorker('reload-table');
+    //isReorderActive = false;
   }
 
    ise.events.onEvent("load-kba-file",(kbaData)=>{
@@ -71,7 +84,7 @@
         kbaRow.setAttribute("id",i);
         
         let kbaRowContent = kbaRows[i].split(",");
-        kbaRow.innerHTML = "<td><button class=\"btn btn-outline-secondary  btn-sm\" onclick=\"removeRow("+i+")\"><span class=\"iconTrash\"></span></button></td><td><a style=\"text-decoration:none;\" id=\"kba-name-id-"+i+"\" title=\""+kbaRowContent[1]+"\" href=\""+kbaRowContent[2]+"\">"+kbaRowContent[0]+" - "+kbaRowContent[1].slice(0,67)+((kbaRowContent[1].length >67) ? "..." : "")+"</a></td><td><button class=\"btn btn-outline-secondary btn-sm\" onclick=\"copyKbaLink("+kbaRowContent[0]+")\"><span class=\"iconCopy\"></span><br> Ext. Link</button></td><td><button class=\"btn btn-outline-secondary btn-sm\" onclick=\"copyKbaId("+i+")\"><span class=\"iconCopy\"></span><br> KBA ID</button></td><td><button class=\"btn btn-outline-secondary btn-sm\" onclick=\"copyKbaIdAndName("+i+")\"><span class=\"iconCopy\"></span><br>ID+Name</button></td>"
+        kbaRow.innerHTML = "<td><button id=\"btn-delete-"+i+"\" class=\"btn btn-outline-secondary btn-sm\" onclick=\"removeRow("+i+")\"><span class=\"iconTrash\"></span></button></td><td><a style=\"text-decoration:none;\" id=\"kba-name-id-"+i+"\" title=\""+kbaRowContent[1]+"\" href=\""+kbaRowContent[2]+"\">"+kbaRowContent[0]+" - "+kbaRowContent[1].slice(0,67)+((kbaRowContent[1].length >67) ? "..." : "")+"</a></td><td><button class=\"btn btn-outline-secondary btn-sm\" onclick=\"copyKbaLink("+kbaRowContent[0]+")\"><span class=\"iconCopy\"></span><br> Ext. Link</button></td><td><button class=\"btn btn-outline-secondary btn-sm\" onclick=\"copyKbaId("+i+")\"><span class=\"iconCopy\"></span><br> KBA ID</button></td><td><button class=\"btn btn-outline-secondary btn-sm\" onclick=\"copyKbaIdAndName("+i+")\"><span class=\"iconCopy\"></span><br>ID+Name</button></td>"
         
         windowKbaTableContent.appendChild(kbaRow);
         
@@ -98,8 +111,6 @@
     if(kbaSplit.length<2){
       kbaSplit = document.getElementById("newKbaInput").value.split("-");
     }
-    
-    console.log(kbaSplit[0]+","+kbaSplit[1]);
 
     if(kbaSplit.length>1){
     //create CSV entry, update CSV file and reload table
@@ -109,8 +120,78 @@
     document.getElementById("kba-format-error").style.display = "none";
 
     }else{
+      //display error message if KBA format is not correct
       document.getElementById("kba-format-error").style.display = "block";
     }
 
     document.getElementById("newKbaInput").value = "";
+   }
+
+   function menuDropdown(){
+    document.getElementById("menuDropdown").classList.toggle("show");
+   }
+
+   function toggleReorder(){
+
+      if(isReorderActive == false){
+        let kbaTable = document.getElementById("scratchpad-kbas").rows;
+        let i;
+        for (i=0; i<kbaTable.length; i++){
+          //create container and reorder buttons
+          let reorderButtonsDiv = document.createElement("div");
+          let reorderButtonUp = document.createElement("button");
+          let reorderButtonDown = document.createElement("button");
+
+          //set container
+          reorderButtonsDiv.setAttribute("style","heigth:100%;");
+
+          //set buttons
+          reorderButtonUp.innerHTML = "↑";
+          reorderButtonDown.innerHTML = "↓";
+          
+          reorderButtonUp.setAttribute("class","btn btn-outline-light btn-sm");
+          reorderButtonUp.setAttribute("style","margin-bottom:2%;");
+          reorderButtonDown.setAttribute("class","btn btn-outline-light btn-sm");
+          reorderButtonDown.setAttribute("style","margin-top:2%;");
+
+          reorderButtonUp.setAttribute("onclick","reorder("+i+",\"up\")");
+          reorderButtonDown.setAttribute("onclick","reorder("+i+",\"down\")");
+
+          //append to container
+          reorderButtonsDiv.appendChild(reorderButtonUp);
+          reorderButtonsDiv.appendChild(reorderButtonDown);
+
+          kbaTable[i].appendChild(reorderButtonsDiv);
+        }
+        isReorderActive = true;
+      }else{
+        ise.extension.sendEventToWorker('update-csv',currentKbaData);
+        ise.extension.sendEventToWorker('reload-table');
+        isReorderActive = false;
+      }
+   }
+
+   function reorder(id,direction){
+    if(direction == "up"){
+      //first row cannot be moved up
+      if(id!=0){
+        let temp = currentKbaData[id-1];
+        currentKbaData[id-1] = currentKbaData[id];
+        currentKbaData[id] = temp;
+      }
+    }else{
+      //last row cannot be moved down
+      if(id!=(currentKbaData.length-1)){
+        let temp = currentKbaData[id+1];
+        currentKbaData[id+1] = currentKbaData[id];
+        currentKbaData[id] = temp;
+      }
+    }
+
+    ise.extension.sendEventToWorker('update-csv',currentKbaData);
+    ise.extension.sendEventToWorker('reload-table');
+    //reloading table removes the reorder elements, this syncs the state and toggles back reordering
+    isReorderActive = false;
+    toggleReorder();
+
    }
